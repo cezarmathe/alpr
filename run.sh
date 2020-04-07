f318d49a8846027b88cf74e4e853368ad3850e07
cezarmathe <me@cezarmathe.com>
2020-04-07 11:18:45 +0300

    Simple bash script

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-ls -d */ > package-list.txt
+ls -d src/*/ > package-list.tmp
 
-aur build -s -v -a package-list.txt
+aur build -s -v -a package-list.tmp
+
+rm package-list.tmp
+
+aur sync $(cat aur-package-list.txt)
