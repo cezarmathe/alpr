accaf24a4220c398348571c424352ae272f36ee9
cezarmathe <me@cezarmathe.com>
2020-04-08 22:43:36 +0300

    Fix init_local_repo

--- scripts/init_local_repo.sh
+++ scripts/init_local_repo.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 
-### Initialize the local repository
+# Initialize the local repository
 
 source "${HOME}/.config/alppr.conf" || exit 1
 
+printf "%s\n" "sudo install -d ${ALPPR_REPO_DIR} -o $USER"
 sudo install -d "${ALPPR_REPO_DIR}" -o $USER
-repo-add "${ALPPR_REPO_DIR}"
+
+printf "%s\n" "repo-add ${ALPPR_REPO_DIR}/${ALPPR_REPO_NAME}"
+repo-add "${ALPPR_REPO_DIR}/${ALPPR_REPO_NAME}"
