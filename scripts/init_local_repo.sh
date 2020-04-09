d923f761235d1521e9e99d37a5058b44239ac473
cezarmathe <me@cezarmathe.com>
2020-04-09 12:54:46 +0300

    Change configuration for repo name and repo file

--- scripts/init_local_repo.sh
+++ scripts/init_local_repo.sh
@@ -7,5 +7,5 @@ source "${HOME}/.config/alppr.conf" || exit 1
 printf "%s\n" "sudo install -d ${ALPPR_REPO_DIR} -o $USER"
 sudo install -d "${ALPPR_REPO_DIR}" -o $USER
 
-printf "%s\n" "repo-add ${ALPPR_REPO_DIR}/${ALPPR_REPO_NAME}"
-repo-add "${ALPPR_REPO_DIR}/${ALPPR_REPO_NAME}"
+printf "%s\n" "repo-add ${ALPPR_REPO_DIR}/${ALPPR_REPO_FILE}"
+repo-add "${ALPPR_REPO_DIR}/${ALPPR_REPO_FILE}"
