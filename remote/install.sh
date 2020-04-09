fd415b7d33be7c62fe3d8432261e1bcbc300c1a6
cezarmathe <me@cezarmathe.com>
2020-04-09 13:35:33 +0300

    Fix remote install script

--- remote/install.sh
+++ remote/install.sh
@@ -3,5 +3,5 @@
 sudo mkdir /var/www/alppr
 sudo chown ${USER} /var/www/alppr
 sudo chgrp ${USER} /var/www/alppr
-sudo ln -s "$(pwd)/alppr.conf" /etc/nginx/conf.d/
+sudo ln -s "$(pwd)/alppr-fileserver.conf" /etc/nginx/conf.d/
 sudo systemctl restart nginx
