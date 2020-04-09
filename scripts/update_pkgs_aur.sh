fd251fe365cebe39a7751ee97d52eca71d74bbce
cezarmathe <me@cezarmathe.com>
2020-04-09 12:33:50 +0300

    Finish aur packages scripts

---
+++ scripts/update_pkgs_aur.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Update AUR packages
+
+source "${HOME}/.config/alppr.conf" || exit 1
+
+PKG_LIST=$(cat ${ALPPR_SRC_DIR}/aur-package-list.txt)
+printf "%s\n%s\n" "Package list:" "${PKG_LIST}"
+
+function aur_sync() {
+    local pkg_name
+    pkg_name="$1"; shift
+
+    aur sync \
+        --sign \
+        --chroot \
+        --directory="${ALPPR_BUILD_ENV_DIR}" \
+        --no-view \
+        --no-confirm \
+        --upgrades \
+        "${pkg_name}"
+}
+
+for pkg_name in ${PKG_LIST}; do
+    aur_sync "${pkg_name}"
+done
